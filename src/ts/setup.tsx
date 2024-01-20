4d48bca3e4e4516cb49c835a09b56de119c72133
AC1556 <git@urbanlife.mozmail.com>
2024-01-20 16:19:43 +0900

    Removing unused imports and changing lifecycle hooks

--- src/ts/setup.tsx
+++ src/ts/setup.tsx
@@ -1,46 +1,49 @@
-import Greeter from '../components/Greeter/Greeter';
-import ModData from '../data/data.json';
-import '../css/styles.css';
-import '../img/icon.png';
-import LargeIcon from '../img/icon_large.png';
-import { MoveToShareBankFormElement, selectedBankItemSig } from '../components/MoveToShareBankFormElement';
-import { ShareBank } from '../components/ShareBank';
-
-import { h, render } from 'preact'
-import { html } from 'htm/preact'
-
-function App(props: any) {
-  return html`<h1>Hello ${props.name}!</h1>`;
-}
-
+import { h, render } from "preact";
+import {
+  MoveToShareBankForm,
+  selectedBankItem,
+} from "../components/MoveToShareBankFormElement";
+import { ShareBank } from "../components/ShareBank";
+import ModData from "../data/data.json";
+import "../css/styles.css";
+import "../img/icon.png";
 
 export async function setup(ctx: Modding.ModContext) {
   await ctx.gameData.addPackage(ModData);
 
-  ctx.onModsLoaded(() => {
-    const root = document.createElement('div');
+  ctx.patch(BankTabMenu, "initialize").after(() => {
+    const root = document.createElement("div");
     render(<ShareBank ctx={ctx} />, root);
 
-    sidebar.category('Clan').item('Clan Bank', {
-      icon: ctx.getResourceUrl('img/icon.png'),
+    const openShareBankRoot = document.createElement("div");
+    render(
+      <button class="btn btn-info" onClick={() => open(ctx, root)}>
+        Share Bank
+      </button>,
+      openShareBankRoot,
+    );
+
+    bankTabMenu.appendChild(openShareBankRoot)
+
+    sidebar.category('Shared').item('Shared Bank', {
       onClick() {
         open(ctx, root);
       },
     });
+  })
+
+  ctx.patch(BankItemSettingsMenu, "setItem").after((_, item, game) => {
+    selectedBankItem.value = item;
   });
 
-  ctx.patch(BankItemSettingsMenu, 'setItem').after((_, item, game) => {
-    console.log('the bank item changed >>>', item);
-    selectedBankItemSig.value = item;
-  })
+  ctx.patch(BankItemSettingsMenu, "setUnselected").after(() => {
+    selectedBankItem.value = null;
+  });
 
-  ctx.patch(BankItemSettingsMenu, 'setUnselected').after(() => {
-    console.log('the bank item was unselected >>>');
-    selectedBankItemSig.value = null;
-  })
-
-  ctx.patch(BankItemSettingsMenu, 'initialize').after((returnValue, game) => {
-    bankSideBarMenu.appendChild(new MoveToShareBankFormElement(ctx).instance)
+  ctx.patch(BankItemSettingsMenu, "initialize").after((returnValue, game) => {
+    const root = document.createElement("div");
+    render(<MoveToShareBankForm ctx={ctx} />, root);
+    bankSideBarMenu.appendChild(root);
   });
 }
 
