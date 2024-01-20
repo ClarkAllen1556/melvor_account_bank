b70b9899c0d1675d942eb73c8ba5565da0f2fc24
AC1556 <git@urbanlife.mozmail.com>
2024-01-20 19:46:15 +0900

    Will now use account storage and improved UX

--- src/components/MoveToShareBankFormElement/MoveToShareBankFormElement.tsx
+++ src/components/MoveToShareBankFormElement/MoveToShareBankFormElement.tsx
@@ -1,72 +1,143 @@
-import { effect, signal } from "@preact/signals";
+import { signal } from "@preact/signals";
+import { currentStorageQty, sharedStorage } from "../ShareBank";
 
-export const selectedBankItemSig = signal<BankItem | null>(null);
+export const selectedBankItem = signal<BankItem | null>(null);
+export const storageKey = `mlv-item-shared-items` as const;
+export const MAX_SHARE_AMOUNT = 10;
+
+export function writeToStorage(items: StoredItems, ctx: Modding.ModContext) {
+  ctx.accountStorage.setItem(storageKey, items);
+
+  localStorage.setItem(
+    storageKey,
+    JSON.stringify(items),
+  );
+}
+
+const transferQuantity = signal<number>(0);
+const isShareFull = signal<boolean>(
+  currentStorageQty.value >= MAX_SHARE_AMOUNT,
+);
 
 export function MoveToShareBankForm({ ctx }: { ctx: Modding.ModContext }) {
-  const transferQuantity = signal<number>(0);
-
   function transferItems() {
-    if (!selectedBankItemSig.value?.item) return;
+    if (!selectedBankItem.value?.item) return;
     if (transferQuantity.value <= 0) return;
-    if (transferQuantity.value > selectedBankItemSig.value.quantity) return;
+    if (transferQuantity.value > selectedBankItem.value.quantity) return;
 
     const item: StoredItem = {
-      id: selectedBankItemSig.value.item.id,
+      id: selectedBankItem.value.item.id,
       qty: transferQuantity.value,
       source: game.characterName,
     };
-    const storageKey = `mlv-item-shared-items` as const;
-    let storedItems = JSON.parse(localStorage.getItem(storageKey)) as {
-      [K: StoredItem["id"]]: StoredItem;
-    };
+    let storedItems: StoredItems = JSON.parse(localStorage.getItem(storageKey));
+    storedItems.createdAt = new Date();
 
-    if (storedItems) {
-      if (storedItems[item.id]) {
-        item.qty += storedItems[item.id].qty;
-      } else {
-        storedItems[item.id] = item;
-      }
+    if (storedItems[item.id]) {
+      storedItems[item.id].qty += item.qty;
     } else {
-      storedItems = {
-        [item.id]: item,
-      };
+      storedItems[item.id] = item;
     }
 
-    localStorage.setItem(
-      `mlv-item-shared-items`,
-      JSON.stringify(storedItems),
-    );
+    writeToStorage(storedItems, ctx);
 
     game.bank.removeItemQuantity(
-      selectedBankItemSig.value.item,
+      selectedBankItem.value.item,
       transferQuantity.value,
       false,
     );
+
+    sharedStorage.value = { ...storedItems };
   }
 
-  if (!selectedBankItemSig.value) return null;
+  function setTransferAmountByPercentage(p: number) {
+    const franctionalAmount = Math.floor(selectedBankItem.value.quantity * p);
+
+    transferQuantity.value = franctionalAmount;
+
+    console.log("frac >>", franctionalAmount);
+    console.log("trans qty >>", transferQuantity.value);
+  }
+
+  if (!selectedBankItem.value) return null;
 
   return (
     <div class="col-12">
       <div class="block block-rounded-double bg-combat-inner-dark">
         <div class="block-header block-header-default bg-dark-bank-block-header px-3 py-1">
-          <h5 class="font-size-sm font-w600 mb-0">Send item(s) to shared bank</h5>
+          <h5 class="font-size-sm font-w600 mb-0">
+            Send item(s) to shared bank
+          </h5>
         </div>
         <div class="col-12 pt-2">
           <div>
-            {selectedBankItemSig.value?.item?.name}
+            {selectedBankItem.value?.item?.name}
           </div>
           <input
             class="form-control m-1"
             type="number"
             placeholder="quantity"
+            value={transferQuantity.value}
             onChange={(e) =>
               transferQuantity.value = Number(
-                (e.target as HTMLInputElement).value,
+                (e.currentTarget as HTMLInputElement).value,
               )}
           />
           <div>
-            <button class="btn btn-danger" onClick={ () => transferItems() }>Send</button>
+            <div>
+              {isShareFull.value && (
+                <div>
+                  <strong>Shared storage is full.</strong>
+                </div>
+              )}
+              <div>
+                <strong>{currentStorageQty.value} / {MAX_SHARE_AMOUNT}</strong>
+              </div>
+            </div>
+
+            <div style={{ display: "flex", gap: "6px" }}>
+              <button
+                disabled={isShareFull.value}
+                class="btn btn-info"
+                style={{
+                  marginBlock: "6px",
+                }}
+                onClick={() => setTransferAmountByPercentage(.25)}
+              >
+                25%
+              </button>
+              <button
+                disabled={isShareFull.value}
+                class="btn btn-info"
+                style={{
+                  marginBlock: "6px",
+                }}
+                onClick={() => setTransferAmountByPercentage(.5)}
+              >
+                50%
+              </button>
+              <button
+                disabled={isShareFull.value}
+                class="btn btn-info"
+                style={{
+                  marginBlock: "6px",
+                }}
+                onClick={() => setTransferAmountByPercentage(1)}
+              >
+                100%
+              </button>
+              <button
+                disabled={isShareFull.value}
+                class="btn btn-danger"
+                style={{
+                  marginBlock: "6px",
+                  marginLeft: "auto",
+                }}
+                onClick={() => transferItems()}
+              >
+                Send
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- src/components/ShareBank/ShareBank.tsx
+++ src/components/ShareBank/ShareBank.tsx
@@ -1,62 +1,98 @@
 import { effect, signal } from "@preact/signals";
-import { html } from "htm/preact";
-import { h, render } from "preact";
+import {
+  MAX_SHARE_AMOUNT,
+  storageKey,
+  writeToStorage,
+} from "../MoveToShareBankFormElement";
 
-function readSharedItems() {
-  const storageKey = `mlv-item-shared-items` as const;
-  const storedItems = JSON.parse(localStorage.getItem(storageKey)) as {
-    [K: StoredItem["id"]]: StoredItem;
-  };
+export const sharedStorage = signal<StoredItems | null>(null);
+export const currentStorageQty = signal<number>(0);
 
-  if (!storedItems) return {};
+effect(() => {
+  if (!sharedStorage.value) {
+    currentStorageQty.value = 0;
+  } else {
+    currentStorageQty.value = Object.keys(sharedStorage.value).length;
+  }
+});
 
-  return storedItems;
+function readFromStorage(ctx: Modding.ModContext): StoredItems {
+  const accStore: StoredItems = ctx.accountStorage.getItem(storageKey);
+  const localStore: StoredItems = JSON.parse(localStorage.getItem(storageKey));
+
+  if (!accStore) {
+    if (!localStore) return null;
+
+    return { ...localStore };
+  }
+
+  const accStoreLastUpdated = new Date(accStore.createdAt);
+  const localStoreLastUpdated = new Date(localStore.createdAt);
+
+  if (accStoreLastUpdated < localStoreLastUpdated) {
+    ctx.accountStorage.setItem(storageKey, localStore);
+
+    return { ...localStore };
+  }
+
+  return { ...accStore };
+}
+
+interface SharedItemProps {
+  item: AnyItem;
+  qty: number;
+  onClick: (i: AnyItem, qty: number) => void;
 }
 
 function SharedItem(
-  { item, qty, onClick }: {
-    item: AnyItem;
-    qty: number;
-    onClick: (i: AnyItem, qty: number) => void;
-  },
+  { item, qty, onClick }: SharedItemProps,
 ) {
+  if (!item) return null;
+
   return (
-    <div
-      class="bank-item pointer-enabled m-2"
-      role="button"
-      data-item-id={item.id}
-      onClick={() => onClick(item, qty)}
-    >
-      <a role="button">
-        <img class="bank-img p-3" src={item.media} />
-        <div class="font-size-sm text-white text-center in-bank">
-          <small class="badge-pill bg-secondary">{qty}</small>
-        </div>
-      </a>
-    </div>
+    <>
+      <div
+        class="bank-item pointer-enabled m-2"
+        role="button"
+        data-item-id={item.id}
+        onClick={() => onClick(item, qty)}
+      >
+        <a role="button">
+          <img class="bank-img p-3" src={item.media} />
+          <div class="font-size-sm text-white text-center in-bank">
+            <small class="badge-pill bg-secondary">{qty}</small>
+          </div>
+        </a>
+      </div>
+    </>
   );
 }
 
 export function ShareBank({ ctx }: { ctx: Modding.ModContext }) {
-  const storedItems = signal<{ [K: StoredItem["id"]]: StoredItem }>({});
-
-  storedItems.value = readSharedItems();
+  sharedStorage.value = readFromStorage(ctx);
+  const hasSharedItems = !!Object.values(sharedStorage.value);
 
   function handleItemClick(item: AnyItem, qty: number) {
-    console.log(item);
+    game.bank.addItemByID(
+      item.id,
+      qty,
+      false,
+      false,
+      true,
+      true,
+      sharedStorage.value[item.id].source,
+    );
 
-    game.bank.addItemByID(item.id, qty, false, false, true, true, storedItems.value[item.id].source);
-
-    const updatedStoredItems = storedItems.value;
+    const updatedStoredItems: StoredItems = sharedStorage.value;
+    updatedStoredItems.createdAt = new Date();
     delete updatedStoredItems[item.id];
 
-    localStorage.setItem(
-      `mlv-item-shared-items`,
-      JSON.stringify(updatedStoredItems)
-    )
+    writeToStorage(updatedStoredItems, ctx);
 
-    storedItems.value = updatedStoredItems;
+    sharedStorage.value = { ...updatedStoredItems };
   }
+
+  if (!hasSharedItems) return null;
 
   return (
     <>
@@ -64,24 +100,40 @@ export function ShareBank({ ctx }: { ctx: Modding.ModContext }) {
         style={{
           display: "flex",
           flexWrap: "wrap",
-          marginBottom: "6px"
+          marginBottom: "24px",
         }}
       >
-        {Object.values(storedItems.value).map((item: StoredItem, i) => (
-          <div key={i} id={`item-${item.id}`}>
+        {Object.values(sharedStorage.value).map((item: StoredItem, i) => (
+          <div
+            key={i}
+            id={`item-${item.id}`}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
             <SharedItem
               item={game.items.getObjectByID(item.id)}
               qty={item.qty}
-              onClick={(item, qty) =>
-                handleItemClick(item, qty)
-              }
+              onClick={(item, qty) => handleItemClick(item, qty)}
             />
           </div>
         ))}
       </div>
-      <button class="btn btn-danger" onClick={() => storedItems.value = readSharedItems()}>
-        Refresh shared items
-      </button>
+      <div>
+        <p>
+          Current storage:{" "}
+          <strong>{currentStorageQty.value} / {MAX_SHARE_AMOUNT}</strong>
+        </p>
+        <button
+          class="btn btn-danger"
+          onClick={() => sharedStorage.value = readFromStorage(ctx)}
+        >
+          Refresh
+        </button>
+      </div>
     </>
   );
 }
