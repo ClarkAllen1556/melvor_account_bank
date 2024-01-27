c145f16ad8edcf0c429984d3481ab8f15ce6a238
AC1556 <git@urbanlife.mozmail.com>
2024-01-27 12:53:49 +0900

    Abstracting common constants

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+export const DB_ITEMS_TBL = 'items' as const;
+export const DB_USERS_TBL = 'users' as const;
+export const STORAGE_KEY = `mlv-item-shared-items` as const;
+export const MOD_SETTINGS = {
+  DB_HOST: 'account-bank-db-host',
+} as const;
