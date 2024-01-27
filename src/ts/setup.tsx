bb92a1997be30025bdd57de19d452c948668a539
AC1556 <git@urbanlife.mozmail.com>
2024-01-27 12:54:28 +0900

    Adding mod settings and PB intergration

--- src/ts/setup.tsx
+++ src/ts/setup.tsx
@@ -1,12 +1,49 @@
-import { render } from "preact";
+import { render } from 'preact';
 import {
   MoveToShareBankForm,
   selectedBankItem,
 } from "../components/MoveToShareBankFormElement";
 import { ShareBank } from "../components/ShareBank";
 import "../css/styles.css";
+import { MOD_SETTINGS } from "../constants";
+import { dbHost, init, login } from '../api';
 
 export async function setup(ctx: Modding.ModContext) {
+  const savedHostname = ctx.accountStorage.getItem(MOD_SETTINGS.DB_HOST);
+
+  if (savedHostname?.trim()) {
+    init(savedHostname);
+  }
+
+  ctx.settings.section('General').add([
+    {
+      type: 'text',
+      name: 'account-bank-db-host',
+      label: 'Account bank server URL',
+      hint: 'Which server you would like your bank to be saved too.',
+      default: 'http://localhost:8090',
+      onChange: (cur: string) => {
+        if (!cur.trim()) return;
+
+        if (ctx.accountStorage.getItem(MOD_SETTINGS.DB_HOST)) {
+          ctx.accountStorage.removeItem(MOD_SETTINGS.DB_HOST);
+        }
+
+        ctx.accountStorage.setItem(MOD_SETTINGS.DB_HOST, cur);
+
+        dbHost.value = cur;
+      }
+    },
+    {
+      type: 'button',
+      name: 'account-bank-login',
+      display: 'Login with Discord',
+      onClick: () => {
+        login('discord')
+      }
+    } as Modding.Settings.ButtonConfig,
+  ]);
+
   ctx.patch(BankTabMenu, "initialize").after(() => {
     const root = document.createElement("div");
     render(<ShareBank ctx={ctx} />, root);
@@ -27,7 +64,7 @@ export async function setup(ctx: Modding.ModContext) {
 
     bankOptionsMenu
       .children[0]
-      .querySelector('.row')
+      .querySelector(".row")
       .children[0]
       .children[0]
       .appendChild(openShareBankRoot);
@@ -43,7 +80,7 @@ export async function setup(ctx: Modding.ModContext) {
 
   ctx.patch(BankItemSettingsMenu, "initialize").after((returnValue, game) => {
     const root = document.createElement("div");
-    root.classList.add('col-13')
+    root.classList.add("col-13");
 
     render(<MoveToShareBankForm ctx={ctx} />, root);
 
